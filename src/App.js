6ab78d151cec3b6c14c4f9567b6ae2d4cca01979
anirudh loona <anirudhloona26@gmail.com>
2022-12-20 12:09:59 +0530

    frontend added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ContractStorage from './artifacts/contracts/contractStorage.sol/contractS
 import { create as ipfsHttpClient } from 'ipfs-http-client'
 import uuid from "react-uuid";
 
+import './App.css'
+
 const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 const client = ipfsHttpClient('https://ipfs.infura.io:5001/api/v0')
 
@@ -28,9 +30,11 @@ function App() {
   }
 
   const addInsuranceToBlockChain = async () => {
-    if(storageContract === ""){
+
+    if (storageContract === "") {
       return
     }
+    console.log("i am here")
     // const file = event.target.files[0];
     // if(!file)return
     // let selectedFile = event.target.files;
@@ -52,26 +56,27 @@ function App() {
     //     // Convert data to base64
     //     fileReader.readAsDataURL(fileToLoad);
     // }
-    
+
     if (!typeof window.ethereum !== 'undefined') {
       console.log('i am here.')
       await requestAccount()
       const provider = new ethers.providers.Web3Provider(window.ethereum)
       const signer = provider.getSigner()
       const contract = new ethers.Contract(contractAddress, ContractStorage.abi, signer)
-      
+
       const transaction = await contract.addInsurance(storageContract)
       await transaction.wait();
+      setStorageContract("");
     }
   }
 
-  async function requestAccount(){
-    await window.ethereum.request(({method : 'eth_requestAccounts'}))
+  async function requestAccount() {
+    await window.ethereum.request(({ method: 'eth_requestAccounts' }))
   }
 
   const addInsuranceToIPFS = async (e) => {
     const file = e.target.files[0]
-    if(!file)return
+    if (!file) return
     console.log(file)
 
     console.log(client)
@@ -82,14 +87,66 @@ function App() {
     //   console.log(err)
     // }
 
-    setStorageContract(uuid());
+    setStorageContract(file.name);
 
   }
 
   return (
     <div className="App">
-      <input type='file' accept="application/pdf" placeholder='upload pdf' onChange={addInsuranceToIPFS} />
-      <button onClick={addInsuranceToBlockChain}/>
+      {/* <input type='file' accept="application/pdf" placeholder='upload pdf' onChange={addInsuranceToIPFS} />
+      <button onClick={addInsuranceToBlockChain}/> */}
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <a className="navbar-brand" href="#">Insure</a>
+        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item active">
+              <a className="nav-link" href="#">Home </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Features</a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#">Pricing</a>
+            </li>
+            <li className="nav-item dropdown">
+              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                Dropdown link
+              </a>
+              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                <a className="dropdown-item" href="#">Action</a>
+                <a className="dropdown-item" href="#">Another action</a>
+                <a className="dropdown-item" href="#">Something else here</a>
+              </div>
+            </li>
+          </ul>
+        </div>
+      </nav>
+      <div className="container">
+        <div className="card">
+          <h3>Upload Files</h3>
+          <div className="drop_box">
+            {storageContract==="" ? <><header>
+              <h4>Select File here</h4>
+            </header>
+            <p>Files Supported: PDF</p>
+            <form action="\">
+              <label class="custom-file-upload">
+                <input onChange={addInsuranceToIPFS} accept="application/pdf" type="file" />
+                Custom Upload 
+              </label> 
+              <br />
+              <button onClick={addInsuranceToBlockChain} className="btn">Submit</button>
+            </form> </>: <>
+            {storageContract}
+            <button onClick={addInsuranceToBlockChain} className="btn">Submit</button>
+            </>}
+          </div>
+
+        </div>
+      </div>
     </div>
   );
 }
